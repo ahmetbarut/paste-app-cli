6a19cb39fc1e31c6dc5bca3ac6a43bde9ff60d17
Ahmet Barut <ahmetbarut588@gmail.com>
2023-10-15 17:29:14 +0300

    up

--- paste.rb
+++ paste.rb
@@ -1,14 +1,36 @@
-class Paste < Formula
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PasteApp < Formula
     desc ""
     homepage ""
-    url "https://github.com/ahmetbarut/paste-app-cli/blob/master/build/paste.phar"
-    version "0.2"
-    sha256 "188e7fcb1d7c46564642d4ca776c739871b76836b4d76a8c9e5478ae70cce47f"
+    url "https://github.com/ahmetbarut/homebrew-paste/blob/main/build/paste.phar"
+    version "1.0"
+    sha256 "0f7dc8c4c09233ef2e3789aaa9e21ea770263a036281105a818ad1d8afa0c0f4"
     license ""
-
+  
+    # depends_on "cmake" => :build
+  
     def install
-        bin.install "paste.phar" => "paste-app"
-        system "./configure", *std_configure_args, "--disable-silent-rules"
+      bin.install "paste.phar" => "paste-app"
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test paste-app`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
     end
   end
   
